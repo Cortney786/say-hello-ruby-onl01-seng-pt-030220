c15e1609a1a7dca4b3ce7c164d9c403b21e62046
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-16 20:06:25 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
 
 # I call on the method say_hello, and give it the string ""
-# The method prints "Hello(name"
+# The method prints "Hello(name)"
 
 def say_hello(name= "Ruby Programmer")
   puts "Hello #{name}!"
